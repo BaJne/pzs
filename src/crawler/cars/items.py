1bf7f5ec3e4f5690e7399e75f72eaf94515f1495
Branislav Jevtic <branislavgrom@gmail.com>
2022-07-18 17:31:06 +0200

    Added new attributes to be extracted by scraping

--- src/crawler/cars/items.py
+++ src/crawler/cars/items.py
@@ -7,8 +7,33 @@ import scrapy
 
 
 class CarsItem(scrapy.Item):
-    # define the fields for your item here like:
-    name = scrapy.Field()
-    price = scrapy.Field()
-    production_year = scrapy.Field()
+    # TEHNICKI PODACI
+    naziv = scrapy.Field()
+    cena = scrapy.Field()
+    godiste = scrapy.Field()
+    gorivo = scrapy.Field()
 
+    kubikaza = scrapy.Field()
+    snaga = scrapy.Field()
+    kilometraza = scrapy.Field()
+    tip_motora = scrapy.Field()
+    pogon = scrapy.Field()
+    tip_menjaca = scrapy.Field()
+    broj_brzina = scrapy.Field()
+    broj_vrata = scrapy.Field()
+    broj_sedista = scrapy.Field()
+    pozicija_volana = scrapy.Field()
+    klima = scrapy.Field()
+    boja = scrapy.Field()
+    boja_unutrasnjosti = scrapy.Field()
+    kategorija_vozila = scrapy.Field()
+
+    prosecna_potrosnja = scrapy.Field() # broj l|100km
+    ubrzanje = scrapy.Field()
+    prtljaznik = scrapy.Field()
+
+    # Istorija vozila
+    servisna_knjizica = scrapy.Field()
+    havarisano = scrapy.Field()
+    garaziran = scrapy.Field()
+    nov = scrapy.Field()
--- src/crawler/cars/spiders/car_spider.py
+++ src/crawler/cars/spiders/car_spider.py
@@ -24,28 +24,68 @@ class CarSpider(scrapy.Spider):
             else:
                 print('ERROR link is null')
 
-        # bttn = response.xpath("//a[@title='Sledeća stranica']").xpath("@href")[0].get()
-        # print(bttn)
+        next_page = response.css("div.pagination").xpath("//a[@class='pag_next']").xpath("@href")[0].get()
+        print(next_page)
 
-        # if m_bttn.css("::text")[0].extract().strip() == "Следеће":
-        #     next_page = m_bttn.xpath("@href")[0].extract()
-        #     print(next_page)
-        #     yield response.follow(next_page, callback=self.parse)
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
 
     def parseItem(self, response, **keywords):
         if response is None:
             print('ERROR')
 
         item = CarsItem()
-        item['name'] = response.css("div.singleOverview h1::text")[0].extract()
-        item['price'] = response.css(".sidebarPrice").css("span.priceReal::text")[0].extract()
+        item['naziv'] = response.css("div.singleOverview h1::text")[0].extract()
+        item['cena'] = response.css(".sidebarPrice").css("span.priceReal::text")[0].extract()
 
-        info_divs = response.css("singleBoxPanel")
+        temp_div = response.css(".basicSingleData")
+        item['godiste'] = temp_div[0].css("li")[1].css("span::text").extract_first()
+        item['gorivo'] = temp_div[0].css("li")[3].css("span::text")[1].get()
 
-        # item['production_year'] = container.xpath("//a[@title='Преузимање']").xpath("@href")[0].extract()
-        item['production_year'] = response.css(".basicSingleData")[0].css("li")[1].css("span::text").extract_first()
+        tech_divs = response.css("ul.techSpec")
 
-        yield item
+        lis1 = tech_divs[0].css("li")
+        for index, li in enumerate(lis1):
+            value = li.css("strong::text").get()
+
+            attributes = {
+                0: 'kubikaza',
+                1: 'snaga',
+                2: 'kilometraza',
+                4: 'tip_motora',
+                5: 'pogon',
+                6: 'tip_menjaca',
+                7: 'broj_brzina',
+                8: 'broj_vrata',
+                9: 'broj_sedista',
+                10: 'pozicija_volana',
+                11: 'klima',
+                12: 'boja',
+                13: 'boja_unutrasnjosti',
+                14: 'kategorija_vozila'
+            }
+
+            att = attributes.get(index, "none")
+            if att != "none":
+                item[att] = value
+
+        for m in response.css("iframe"):
+            rating_div = m.xpath("@src").get()
+            yield response.follow(rating_div, callback=self.get_rating, cb_kwargs=item)
+
+    def get_rating(self, response, **keywords):
+        ratings = response.css("div.car-preview-rating-value::text")
+
+        if len(ratings) < 3:
+            yield keywords
+            return
+
+        keywords['prosecna_potrosnja'] = ratings[0].get().strip()
+        keywords['ubrzanje'] = ratings[2].get().strip()
+        keywords['prtljaznik'] = ratings[4].get().strip()
+
+        yield keywords
+
 
 # XPATH scraping
 #   response.xpath("//title/text()").extract()
